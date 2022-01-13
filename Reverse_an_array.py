30f3ef3691c221877a744fca210b00dc3ba78e26
Bhanu PS Chauhan <75789100+Coder-Chauhan@users.noreply.github.com>
2022-01-13 20:45:09 +0530

    Add files via upload

---
+++ Reverse_an_array.py
@@ -0,0 +1,15 @@
+# fucntion for reversing the list
+def reverse_array(li) :
+    li.reverse()
+    return li
+
+n = int(input("Enter the length of the array : "))
+print("Enter the list")
+li = []
+
+for i in range(n):
+    ele = int(input())
+    li.append(ele)
+
+print(li)
+print(reverse_array(li))
