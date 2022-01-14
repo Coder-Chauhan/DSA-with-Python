0b81dfb944099818a286f8a9739ac72ac44ab13a
Bhanu PS Chauhan <75789100+Coder-Chauhan@users.noreply.github.com>
2022-01-14 13:19:12 +0530

    Add files via upload

---
+++ negative_numbers_to_one_side.py
@@ -0,0 +1,20 @@
+def rearrange(arr,n):
+    j = 0
+    for i in range(n):
+        if arr[i] < 0:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            j+=1
+    return arr
+
+n = int(input("Enter the length of the array : "))
+print("Enter the elements : ")
+arr = []
+
+for i in range(n) :
+    ele = int(input())
+    arr.append(ele)
+
+print(arr)
+print(rearrange(arr,n))
