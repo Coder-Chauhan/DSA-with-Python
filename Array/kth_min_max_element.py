89cf69a6e64c913a1a9ba5f025b6c03a28980efe
Bhanu PS Chauhan <75789100+Coder-Chauhan@users.noreply.github.com>
2022-01-17 20:05:46 +0530

    Add files via upload

---
+++ Array/kth_min_max_element.py
@@ -0,0 +1,19 @@
+# Program to find the kth minimum and maximum number in an array
+def min_max(li,k,n) :
+    li.sort()
+    print(f"{k} Maximum element is :", li[n-k-1])
+    print(f"{k} Minimum element is :", li[k-1])
+    return
+
+n = int(input("Enter the length of the array : "))
+print("Enter the array")
+li = []
+
+for i in range(n):
+    ele = int(input())
+    li.append(ele)
+
+k = int(input("Enter the kth number"))
+
+print("List :", li)
+print(min_max(li,k,n))
---
+++ Array/min_and_max_element_in_an_array.py
@@ -0,0 +1,16 @@
+# Program to find the minimum and maximum number in an array
+def min_max(li) :
+    print("Maximum element is :", max(li))
+    print("Minimum element is :", min(li))
+    return
+
+n = int(input("Enter the length of the array : "))
+print("Enter the array")
+li = []
+
+for i in range(n):
+    ele = int(input())
+    li.append(ele)
+
+print("List :", li)
+print(min_max(li))
