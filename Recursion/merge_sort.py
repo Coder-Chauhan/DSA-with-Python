3a90abdb1f3782cb7efc77de8c3119fd4918daad
Bhanu PS Chauhan <75789100+Coder-Chauhan@users.noreply.github.com>
2022-01-20 03:15:41 +0530

    Add files via upload

---
+++ Recursion/merge_sort.py
@@ -0,0 +1,37 @@
+def merge(s1,s2,arr):
+    i = 0
+    j = 0
+    k = 0
+    while i < len(s1) and j < len(s2):
+        if s1[i] < s2[j]:
+            arr[k] = s1[i]
+            i += 1
+            k += 1
+        else :
+            arr[k] = s2[j]
+            j += 1
+            k += 1
+    while i < len(s1):
+        arr[k] = s1[i]
+        i += 1
+        k += 1
+    while j < len(s2):
+        arr[k] = s2[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr):
+    if len(arr)==0 or len(arr)==1:
+        return
+    mid = len(arr) // 2
+    s1 = arr[0:mid]
+    s2 = arr[mid:]
+    merge_sort(s1)
+    merge_sort(s2)
+    print(s1)
+    print(s2)
+    merge(s1,s2,arr)
+
+arr = [10,5,3,1,7,9,4]
+merge_sort(arr)
+print(arr)
